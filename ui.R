e95e53d10fb3160e5a878328c74040e677348f2f
Fattigman <jacob.karlstrom@gmail.com>
2024-05-15 21:00:27 +0200

    Actual actual changes

--- ui.R
+++ ui.R
@@ -6,14 +6,19 @@ mP <- mainPanel(
 
 sP <- sidebarPanel(
   # Create an input for predictor variables
-  ,
-  # Create an input for response variable
-  
+  parameter <- c("red_wine", "white_wine")
 )
 
 # Define UI
 ui <- fluidPage(
   titlePanel("Wine Quality Regression Analysis"),
+  sidebar = sidebar("Quality parameters"),
+  card(),
+  selectInput(
+    "parameter",
+    "Select option",
+    choices = list("Red wine" = 1, "White wine" = 2),
+    selected = 1), 
   sidebarLayout(
     sP,
     mP
